eb816236cacf993a13afac6ed7587915642ffcb6
Henrique Padilha Balbino <93481091+henriquepadilhaprogramer@users.noreply.github.com>
2021-11-23 20:39:42 -0300

    Add files via upload

---
+++ URI1003resolvido.cs
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+URI 1003 RESOLVIDO - SOMA SIMPLES
+
+Leia dois valores inteiros, no caso para variáveis A e B. A seguir,
+calcule a soma entre elas e atribua à variável SOMA. 
+A seguir escrever o valor desta variável.
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "SOMA" com todas as letras maiúsculas,
+com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B.
+Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, 
+caso contrário, você receberá "Presentation Error".
+
+*******************************************************************************/
+using System;
+
+namespace URI1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declarando as variavéis.
+            int A, B , SOMA ;
+
+             A = int.Parse(Console.ReadLine());
+             B = int.Parse(Console.ReadLine());
+
+            // Cálculo da Soma.
+            SOMA = A + B;
+
+            // entrada de valores;
+
+            Console.WriteLine("SOMA = "+ SOMA);
+
+        }
+    }
+}
